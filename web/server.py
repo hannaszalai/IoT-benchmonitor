1b0f00b73fa92135060a138358f1b526165b8cd1
Hanna Szalai <hs223xt@student.lnu.se>
2025-07-01 17:15:16 +0200

    server file improved

--- web/server.py
+++ web/server.py
@@ -3,15 +3,28 @@ from flask import Flask, jsonify, send_from_directory
 from influxdb_client import InfluxDBClient
 import os
 
+
+# ------------------------------------------------------
+# CONFIGURATION
+# ------------------------------------------------------
+
+# Flask app setup - serves static files from ./static
 app = Flask(__name__, static_url_path='/static', static_folder='static')
 
+# InfluxDB client configuration
 INFLUX_URL = "http://localhost:8086"
 INFLUX_TOKEN = "my-token"
 INFLUX_ORG = "my-org"
 INFLUX_BUCKET = "my-bucket"
 
+# Create InfluxDB client and query API
 client = InfluxDBClient(url=INFLUX_URL, token=INFLUX_TOKEN, org=INFLUX_ORG)
 query_api = client.query_api()
+
+
+# ------------------------------------------------------
+# ROUTES
+# ------------------------------------------------------
 
 @app.route("/")
 def index():
@@ -19,6 +32,7 @@ def index():
 
 @app.route("/data")
 def get_data():
+    # Flux query to get the last 10 minutes of bench data
     query = f'''
     from(bucket: "{INFLUX_BUCKET}")
     |> range(start: -10m)
@@ -29,12 +43,14 @@ def get_data():
     |> keep(columns: ["_time", "_field", "_value"])
     '''
 
-
+    # Run the query
     tables = query_api.query(org=INFLUX_ORG, query=query)
     
+    # Prepare data structures for each field
     temp, hum, sun, feels, rain, sitting = [], [], [], [], [], []
     avg_scores, total_reviews = [], []
 
+    # Parse query results into JSON lists
     for table in tables:
         for record in table.records:
             t = record.get_time().isoformat()
@@ -58,6 +74,7 @@ def get_data():
             elif f == "total_reviews":
                 total_reviews.append({ "time": t, "value": v })
 
+    # Return everything as a single JSON object
     return jsonify({
         "temp": temp,
         "hum": hum,
@@ -68,6 +85,11 @@ def get_data():
         "avg_scores": avg_scores,
         "total_reviews": total_reviews,
     })
+
+
+# ------------------------------------------------------
+# ENTRY POINT
+# ------------------------------------------------------
 
 if __name__ == "__main__":
     app.run(debug=True)
